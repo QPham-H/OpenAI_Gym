807fc8c1ea45f9ba862db245357015d868661b5d
Quoc H Pham <quocpham.h@gmail.com>
2022-02-07 14:00:43 -0600

    Update CartPole_v0.py

--- CartPole_v0.py
+++ CartPole_v0.py
@@ -1,8 +1,17 @@
 import gym
 
 env = gym.make('CartPoke-v0')
-env.reset()
-for _ in range(1000):
-  env.render()
-  env.step(env.action_space.sample()) 
+print("Action space of: ", env.action_space)
+print("Observation space of: ", env.observation_space)
+
+for i_episode in range(1000):
+  observation = env.reset()
+  for t in range(100):
+    env.render()
+    print(observation)
+    action = env. action_space.sample()
+    observation, reward, done, info = env.step(action)
+    if done
+      print("Episode finished after {} timesteps".format(t+1))
+      break
  env.close()
