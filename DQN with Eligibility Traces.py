07025255231ec34fb340b5962cd14aa7f6ab2c9c
Quoc H Pham <quocpham.h@gmail.com>
2022-02-13 23:40:30 -0600

    Update DQN with Eligibility Traces.py

--- DQN with Eligibility Traces.py
+++ DQN with Eligibility Traces.py
@@ -9,8 +9,9 @@ Code by Quoc Pham 02.2022
 
 import gym
 import random
+import numpy as np
 
-from tensorflow.keras.layers import BatchNormalization, concatenate, Dense, Dropout, Input, Flatten, MaxPooling2D, Conv2D
+from tensorflow.keras.layers import BatchNormalization, concatenate, Dense, Dropout, Input, Flatten, MaxPooling2D, Conv2D, Rescaling
 from tensorflow.keras.models import Sequential, Model
 
 from collections import deque
@@ -21,17 +22,17 @@ game = 'AirRaid-v0'
 # Constants and Variables
 EPISODES = 10
 STEPS = 100
-EPSILON = 0.1
+EPSILON = 0.2
 ALPHA = 0.1 # Learning rate
 DISCOUNT = 0.9
 LAMBDA = 0.9 # For Eligibility trace
 BATCH = 32
-MEMORY = BATCH * 15
+MEMORY = BATCH * 10
 EPOCHS = 1
 display = True
 
 # Begin creating the environment 
-env = gym.make(game)
+env = gym.make(game) #, render_mode='human') # Using `render_mode` provides access to proper scaling, audio support, and proper framerates
 
 action_space = env.action_space.n # Number of actions
 obs_space = env.observation_space.shape # RGB image of screen (height, width, channels)
@@ -44,17 +45,20 @@ print(f'This is the number of actions: {action_space}')
 print(f'This is the observation image: {obs_space}')
 
 
-input_layer = Input(shape=(img_ht, img_wd,3))
-x = BatchNormalization()(input_layer)
+input_layer = Input(shape=(img_ht, img_wd,3),name='image')
+x = Rescaling(1/255)(input_layer)
+x = BatchNormalization()(x)
 x = Conv2D(8,3,activation='relu',padding='same')(x)
 x = MaxPooling2D(2)(x)
 x = Conv2D(16,3,activation='relu',padding='same')(x)
+x = MaxPooling2D(2)(x)
+x = Conv2D(8,3,activation='relu',padding='same')(x)
 x = MaxPooling2D(2)(x)
 #x = Conv2D(32,3,activation='relu',padding='same')(x)
 #x = MaxPooling2D(2)(x)
 cnn = Flatten()(x)
 
-action_layer = Input(shape=(action_space))
+action_layer = Input(shape=(action_space),name='action')
 
 #x = Dense(1024, activation='relu')(x)
 #x = Dropout(0.3)(x)
@@ -74,13 +78,19 @@ replay_memory = deque()
 q_dict = dict()
 
 def action_selection(state):
+    state = np.expand_dims(state,axis=0)    # Need this to use predict on one sample
     
-    q_values = np.zeroes(action_space)
+    q_values = np.zeros(action_space)
     for a in range(action_space):
-        one_hot_action = np.zeroes(action_space)
+        one_hot_action = np.zeros(action_space)
         one_hot_action[a] = 1
 
-        q_values[a] = q_model.call([state / 255], one_hot_action)
+        one_hot_action = np.expand_dims(one_hot_action,axis=0)
+        
+##        print(f'This is state shape: {state.shape}')
+##        print(f'This is action shape: {one_hot_action.shape}')
+        
+        q_values[a] = q_model.predict([state, one_hot_action])
         
     if random.random() <= EPSILON:
         action = random.randint(0, action_space - 1)
@@ -90,13 +100,16 @@ def action_selection(state):
     return action, q_values[action]
 
 def propagate_delta(curr_q_value, next_state, reward, t):
+    next_state = np.expand_dims(next_state,axis=0)
 
-    next_q_values = np.zeroes(action_space)
+    next_q_values = np.zeros(action_space)
     for a in range(action_space):
-        one_hot_action = np.zeroes(action_space)
+        one_hot_action = np.zeros(action_space)
         one_hot_action[a] = 1
-
-        next_q_values[a] = q_model.call([next_state / 255], one_hot_action)
+        
+        one_hot_action = np.expand_dims(one_hot_action,axis=0)
+        
+        next_q_values[a] = q_model.predict([next_state, one_hot_action])
 
     delta = reward + DISCOUNT * np.max(next_q_values) - curr_q_value
 
@@ -105,7 +118,7 @@ def propagate_delta(curr_q_value, next_state, reward, t):
     
     for i in range(len(replay_memory)):
         n = t - replay_memory[i][5] # where n is the time steps away from current time
-        z = (LAMBDA * DISCOUNT) ^ n
+        z = (LAMBDA * DISCOUNT) ** n
         
         if z <= 0.1:
             break
@@ -121,7 +134,7 @@ def train_model():
     optimizer='adam'
     )
 
-    one_hot_actions = np.zeroes(BATCH, action_space)
+    one_hot_actions = np.zeros(BATCH, action_space)
     for n in range(sessions):
         batch = random.sample(replay_memory, BATCH)
 
@@ -133,7 +146,7 @@ def train_model():
         q_values = [data[2] for data in batch]
 
         history = model.fit(
-            [states,actions],
+            [states, actions],
             q_values,
             batch_size = BATCH,
             epochs = EPOCHS
@@ -153,13 +166,13 @@ for episode in range(EPISODES):
         action, q_value = action_selection(state)
         next_state, reward, done, info = env.step(action)
 
-        replay_memory.appendleft(state, action, q_value, next_state, done, t)
+        replay_memory.appendleft((state, action, q_value, next_state, done, t))
 
         if len(replay_memory) > MEMORY: # To control memory size from exploding
             replay_memory.popright()
 
         if reward != 0: # If there's a reward, give credit to recent states based on eligibility traces
-            propagate_delta(next_state, reward, t)
+            propagate_delta(q_value, next_state, reward, t)
             episode_reward += reward
 
         if done:
